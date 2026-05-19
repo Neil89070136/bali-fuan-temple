62620c203a13e2b5152644d83cfc081d40596cee
Neil Chen <neil89070136@gmail.com>
2026-05-20 02:40:44 +0800

    add get api

--- app/api/ecpay/route.ts
+++ app/api/ecpay/route.ts
@@ -1,5 +1,12 @@
 import { NextResponse } from "next/server";
 
+export async function GET() {
+  return NextResponse.json({
+    status: "success",
+    message: "綠界 API 已成功啟動",
+  });
+}
+
 export async function POST() {
   const merchantID = "3002607";
 
@@ -11,7 +18,7 @@ export async function POST() {
     TotalAmount: "100",
     TradeDesc: "福安寺建寺護持",
     ItemName: "建寺護持",
-    ReturnURL: "https://bali-fuan-temple.vercel.app",
+    ReturnURL: "https://bali-fuan-template.vercel.app",
     ChoosePayment: "ALL",
     EncryptType: "1",
     CheckMacValue: "TEST",
