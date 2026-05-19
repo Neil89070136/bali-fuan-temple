fa899cbb2c50434ae441002d436a2a7d2dfb6dc8
Neil Chen <neil89070136@gmail.com>
2026-05-20 03:50:56 +0800

    fix ecpay sdk

--- app/api/ecpay/route.ts
+++ app/api/ecpay/route.ts
@@ -1,37 +1,44 @@
 import { NextResponse } from "next/server";
 
-const options = {
-  OperationMode: "Test",
-  MercProfile: {
-    MerchantID: "3002607",
-    HashKey: "5294y06JbISpM5x9",
-    HashIV: "v77hoKGq4kWxNNIS",
-  },
-  IgnorePayment: [],
-  IsProjectContractor: false,
-};
+export async function GET() {
+  try {
+    const ecpay_payment = require("ecpay_aio_nodejs");
 
-export async function POST() {
-  const ecpay_payment = require("ecpay_aio_nodejs");
+    const options = {
+      OperationMode: "Test",
+      MercProfile: {
+        MerchantID: "3002607",
+        HashKey: "5294y06JbISpM5x9",
+        HashIV: "v77hoKGq4kWxNNIS",
+      },
+      IgnorePayment: [],
+      IsProjectContractor: false,
+    };
 
-  const create = new ecpay_payment(options);
+    const create = new ecpay_payment(options);
 
-  const order = {
-    MerchantTradeNo: `FUAN${Date.now()}`,
-    MerchantTradeDate: "2026/05/20 12:00:00",
-    TotalAmount: "100",
-    TradeDesc: "福安寺建寺護持",
-    ItemName: "建寺護持",
-    ReturnURL: "https://developers.ecpay.com.tw/",
-    ChoosePayment: "ALL",
-    EncryptType: 1,
-  };
+    const order = {
+      MerchantTradeNo: `FUAN${Date.now()}`,
+      MerchantTradeDate: "2026/05/20 12:00:00",
+      TotalAmount: "100",
+      TradeDesc: "福安寺建寺護持",
+      ItemName: "建寺護持",
+      ReturnURL: "https://developers.ecpay.com.tw/",
+      ChoosePayment: "ALL",
+      EncryptType: 1,
+    };
 
-  const html = create.payment_client.aio_check_out_all(order);
+    const html = create.payment_client.aio_check_out_all(order);
 
-  return new NextResponse(html, {
-    headers: {
-      "Content-Type": "text/html",
-    },
-  });
+    return new NextResponse(html, {
+      headers: {
+        "Content-Type": "text/html",
+      },
+    });
+  } catch (err: any) {
+    return NextResponse.json({
+      error: true,
+      message: err.message,
+    });
+  }
 }
