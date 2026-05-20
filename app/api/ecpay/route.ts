2bafa42ffc4d6ffe1518f467bd0be47616d95eef
Neil Chen <neil89070136@gmail.com>
2026-05-20 17:43:37 +0800

    fix ecpay

--- app/api/ecpay/route.ts
+++ app/api/ecpay/route.ts
@@ -1,36 +1,58 @@
 import { NextResponse } from "next/server";
+
 const ecpay_payment = require("ecpay_aio_nodejs");
 
 export async function GET() {
-  const base_param = {
-    MerchantTradeNo: "FUAN" + Date.now(),
-    MerchantTradeDate: new Date().toLocaleString("sv-SE").replace("T", " "),
-    TotalAmount: "100",
-    TradeDesc: "福安寺捐款",
-    ItemName: "建寺護持",
-    ReturnURL: "https://google.com",
-    ChoosePayment: "ALL",
-    EncryptType: 1,
-  };
+  try {
+    const base_param = {
+      MerchantTradeNo: "FUAN" + Date.now(),
+      MerchantTradeDate: new Date()
+        .toLocaleString("sv-SE")
+        .replace("T", " "),
 
-  const options = {
-    OperationMode: "Test",
-    MercProfile: {
-      MerchantID: "3002607",
-      HashKey: "pwFHCqoQZGmho4w6",
-      HashIV: "EkRm7iFT261dpevs",
-    },
-    IgnorePayment: [],
-    IsProjectContractor: false,
-  };
+      TotalAmount: "100",
 
-  const create = new ecpay_payment(options);
+      TradeDesc: "福安寺捐款",
 
-  const html = create.payment_client.aio_check_out_all(base_param);
+      ItemName: "建寺護持",
 
-  return new NextResponse(html, {
-    headers: {
-      "Content-Type": "text/html",
-    },
-  });
+      ReturnURL: "https://www.google.com",
+
+      ChoosePayment: "ALL",
+
+      EncryptType: 1,
+    };
+
+    const options = {
+      OperationMode: "Test",
+
+      MercProfile: {
+        MerchantID: "3002607",
+
+        HashKey: "pwFHCqoQZGmho4w6",
+
+        HashIV: "EkRm7iFT261dpevs",
+      },
+
+      IgnorePayment: [],
+
+      IsProjectContractor: false,
+    };
+
+    const create = new ecpay_payment(options);
+
+    const html = create.payment_client.aio_check_out_all(base_param);
+
+    return new NextResponse(html, {
+      headers: {
+        "Content-Type": "text/html",
+      },
+    });
+  } catch (err: any) {
+    return NextResponse.json({
+      error: true,
+      message: err.message,
+      stack: err.stack,
+    });
+  }
 }
