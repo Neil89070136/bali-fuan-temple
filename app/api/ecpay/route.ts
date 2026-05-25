c6ae265bc9def5d363fe4081c9239bb487615952
Neil Chen <neil89070136@gmail.com>
2026-05-25 08:19:44 +0800

    use ecpay sdk

--- app/api/ecpay/route.ts
+++ app/api/ecpay/route.ts
@@ -1,64 +1,24 @@
-import { generateCheckMacValue, getEcpayDate } from "@/lib/ecpay";
+import create from "@/lib/ecpay-sdk";
 
 export async function GET() {
-  const params = {
-    MerchantID: "3002607",
-    MerchantTradeNo: "FUAN" + Date.now(),
-    MerchantTradeDate: getEcpayDate(),
+  const MerchantTradeDate = new Date()
+    .toLocaleString("sv-SE", {
+      timeZone: "Asia/Taipei",
+    })
+    .replace("T", " ");
 
-    PaymentType: "aio",
+  const params = {
+    MerchantTradeNo: "FUAN" + Date.now(),
+    MerchantTradeDate,
     TotalAmount: "100",
     TradeDesc: "test",
     ItemName: "donate",
-
     ReturnURL: "https://www.ecpay.com.tw/receive.php",
-
+    ClientBackURL: "https://google.com",
     ChoosePayment: "Credit",
-    IgnorePayment: "",
-
-    EncryptType: "1",
   };
 
-  const result = generateCheckMacValue(params);
-
-  console.log({
-    RAW: result.raw,
-    ENCODED: result.encoded,
-    CHECKMAC: result.checkMacValue,
-  });
-
-  const html = `
-<!DOCTYPE html>
-<html>
-<body>
-
-<form id="ecpay-form"
-      method="post"
-      action="https://payment-stage.ecpay.com.tw/Cashier/AioCheckOut/V5">
-
-${Object.entries({
-  ...params,
-  CheckMacValue: result.checkMacValue,
-})
-  .map(
-    ([key, value]) =>
-      `<input type="hidden" name="${key}" value="${value}" />`
-  )
-  .join("")}
-
-<button type="submit">
-前往付款
-</button>
-
-</form>
-
-<script>
-document.getElementById("ecpay-form").submit();
-</script>
-
-</body>
-</html>
-`;
+  const html = create.payment_client.aio_check_out_all(params);
 
   return new Response(html, {
     headers: {
